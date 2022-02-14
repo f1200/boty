d0a3ad41d6ddd09fbcc691f1fa657662eb9b22a7
f1200 <99683519+f1200@users.noreply.github.com>
2022-02-14 10:58:06 -0800

    Update my.py

--- my.py
+++ my.py
@@ -44,7 +44,7 @@ def retrry(message):
             ch="https://www.youtube.com/watch?v=QUhUxLPbrDo&list=PL-1UAVDVI3ffP10PyEF12HFnLK-MmKBDE"
             bot.send_message(message.chat.id, f"{status} تحتاج للاشتراك في القناة للاستمرار  \n  القناة  @adowat \n \n يوتيوب {ch}")
     except:
-        bot.send_message(message.chat.id, f"/start ارسل مرة اخرى")
+        runn(message)
         
     
 def runn(message):
